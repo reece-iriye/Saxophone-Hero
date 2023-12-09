d8517eb85028d3ee837c3181b7e891a4bdedd57d
Christopher Miller <72897098+ChrisMillerSMU@users.noreply.github.com>
2023-12-08 20:54:57 -0600

    UI Update
    Major

--- xcode/Saxophone-Hero/ViewControllers/LaunchViewController.swift
+++ xcode/Saxophone-Hero/ViewControllers/LaunchViewController.swift
@@ -2,25 +2,49 @@
 import UIKit
 
 class LaunchViewController: UIViewController {
-    override var shouldAutorotate: Bool {
-        return true
-    }
     
-    // Adjusted to comply with the new iOS guidelines
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        // Request geometry update for landscape orientation
-        if let windowScene = view.window?.windowScene {
-            let orientationUpdate = UIWindowScene.GeometryPreferences.iOS(interfaceOrientations: .landscape)
-            windowScene.requestGeometryUpdate(orientationUpdate) { error in
-                print(error.localizedDescription)
-            }
+    @IBOutlet weak var Level1Player1: UILabel!
+    @IBOutlet weak var Level1Player2: UILabel!
+    @IBOutlet weak var Level1Player3: UILabel!
+    @IBOutlet weak var Level1Player4: UILabel!
+    @IBOutlet weak var Level1Player5: UILabel!
+    
+    @IBOutlet weak var Level2Player1: UILabel!
+    @IBOutlet weak var Level2Player2: UILabel!
+    @IBOutlet weak var Level2Player3: UILabel!
+    @IBOutlet weak var Level2Player4: UILabel!
+    @IBOutlet weak var Level2Player5: UILabel!
+    
+    @IBOutlet weak var flame1: UIImageView!
+    @IBOutlet weak var flame2: UIImageView!
+    @IBOutlet weak var flame3: UIImageView!
+    
+    @IBOutlet weak var difficultyLabel: UILabel!
+    
+    @IBAction func setDifficulty(_ sender: UISlider) {
+        let difficulty = Int(sender.value)
+        
+        if(difficulty == 0){
+            flame1.isHidden = true;
+            flame2.isHidden = true;
+            flame3.isHidden = true;
+            difficultyLabel.text = "Difficulty-Easy";
+        }
+        else if(difficulty == 1){
+            flame1.isHidden = false;
+            flame2.isHidden = true;
+            flame3.isHidden = true;
+            difficultyLabel.text = "Difficulty-Medium";
+        }
+        else{
+            flame1.isHidden = false;
+            flame2.isHidden = false;
+            flame3.isHidden = false;
+            difficultyLabel.text = "Difficulty-Hard";
         }
     }
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        return .landscape
-    }
-    override public var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
-        return .landscapeRight
+    
+    override func viewDidLoad() {
+        
     }
 }
